e447ef4b800651c7584310e2f55e47ec0f432af0
Kristina Frederick <kristinafrederick@icloud.com>
2021-03-05 01:08:57 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,6 +16,6 @@ class String
 
   def count_sentences
     #binding.pry
-    self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
+    self.split(/\.|\?|\!/).delete_if {|word| word.size < 2}.size
   end
 end
